6fdfddbf175657bdcd3c8f2de5580500e401b899
Tushar Poddar <tusharpoddar99@gmail.com>
2019-10-13 01:56:38 -0700

    List view with the sub-set

--- app/src/main/java/com/hritikaggarwal/hearout/ToDoList.java
+++ app/src/main/java/com/hritikaggarwal/hearout/ToDoList.java
@@ -5,8 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class ToDoList extends AppCompatActivity {
+
+    private ListView resultsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,37 @@ public class ToDoList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        resultsListView = (ListView) findViewById(R.id.listview);
+
+        // this is the code that is used for 2 lines in a list view
+        HashMap<String, String> nameAddresses = new HashMap<>();
+        nameAddresses.put("Diana", "3214 Broadway Avenue");
+        nameAddresses.put("Tyga", "343 Rack City Drive");
+        nameAddresses.put("Rich Homie Quan", "111 Everything Gold Way");
+        nameAddresses.put("Donna", "789 Escort St");
+        nameAddresses.put("Bartholomew", "332 Dunkin St");
+        nameAddresses.put("Eden", "421 Angelic Blvd");
+
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item,
+                new String[]{"First Line", "Second Line"},
+                new int[]{R.id.text1, R.id.text2});
+
+
+        Iterator it = nameAddresses.entrySet().iterator();
+        while (it.hasNext())
+        {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        resultsListView.setAdapter(adapter);
+
+
     }
 
 
